ed964fec878d425abc81d3e98923c0cf977b3dce
Zav <chaoiZav@gmail.com>
2024-11-04 20:09:06 +0530

    hosted the server in a docker container running in azure vm changed the base_url in retofitClient to azure vm's address

--- app/src/main/java/com/example/mathhelper/RetrofitClient.kt
+++ app/src/main/java/com/example/mathhelper/RetrofitClient.kt
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
     fun getInstance(): Retrofit {
 //        for emulator
-        val BASE_URL = "http://10.0.2.2:5000"
+        val BASE_URL = "http://zaurav.centralindia.cloudapp.azure.com"
 //        for physical device
 //        val BASE_URL = "http://127.0.0.1:5000"
 
